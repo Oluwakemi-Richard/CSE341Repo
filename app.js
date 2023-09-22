e175733087d293b3a936e590feb849c8dd590577
Oluwakemi Richard <52783711+Oluwakemi-Richard@users.noreply.github.com>
2023-09-22 19:30:05 +0100

    week 2 assignment

---
+++ app.js
@@ -0,0 +1,34 @@
+// const express = require('express');
+// const app = express();
+// const port = process.env.PORT || 3000;
+ 
+
+// app.use('/', require('./routes'))
+
+// app.listen(port, () => {
+//     console.log(`Running on port ${port}`)
+//   })
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongodb = require('./db/connect');
+
+const port = process.env.PORT || 8080;
+const app = express();
+
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  })
+  .use('/', require('./routes'));
+
+mongodb.initDb((err, mongodb) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
+  }
+});
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,6 @@ const routes = require('express').Router();
 routes.get('/', (req, res) => {
   res.send('Ajibike Richard');
 });
+routes.use('/contacts', require('./contacts'))
 
 module.exports = routes;
